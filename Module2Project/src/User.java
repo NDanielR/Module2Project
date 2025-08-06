b8845cde31e37675e2e0c6ce8327005a880f71e1
Nelson Daniel Ramirez <n.danielramirez.h@outlook.com>
2025-08-06 11:42:21 -0500

    Add class User

---
+++ Module2Project/src/User.java
@@ -0,0 +1,17 @@
+public class User {
+
+    private String firtName;
+    private String lastName;
+    private Integer id;
+    private String userName;
+    private String password;
+
+    public User(String firtName, String lastName, Integer id, String userName, String password){
+        this.firtName = firtName;
+        this.lastName = lastName;
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+    }
+
+}
